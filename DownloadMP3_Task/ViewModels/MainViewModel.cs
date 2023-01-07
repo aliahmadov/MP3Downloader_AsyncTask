e7902f610e05ba66720aeea64446a2b96547c9e5
Ali Ahmadov <aahmadov7626@ada.edu.az>
2023-01-08 00:48:55 +0400

    view done

---
+++ DownloadMP3_Task/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using DownloadMP3_Task.Commands;
+using DownloadMP3_Task.ViewModels.UCViewModels;
+using DownloadMP3_Task.Views.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DownloadMP3_Task.ViewModels
+{
+    public class MainViewModel:BaseViewModel
+    {
+
+        public WrapPanel WrapPanel { get; set; }
+
+        public RelayCommand AddDownloaderCommand { get; set; }
+
+
+
+        public MainViewModel()
+        {
+            AddDownloaderCommand = new RelayCommand(c =>
+            {
+                var view = new DownloadPanelUC();
+                var viewModel = new DownloadPanelViewModel();
+                view.DataContext = viewModel;
+
+                view.Margin = new Thickness(10, 40, 10, 10);
+
+                WrapPanel.Children.Add(view);
+            });
+        }
+    }
+}
---
+++ DownloadMP3_Task/ViewModels/UCViewModels/DownloadPanelViewModel.cs
@@ -0,0 +1,29 @@
+﻿using DownloadMP3_Task.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadMP3_Task.ViewModels.UCViewModels
+{
+    public class DownloadPanelViewModel:BaseViewModel
+    {
+
+        public RelayCommand WaitCommand { get; set; }
+
+
+        public RelayCommand StartDownloaderCommand { get; set; }
+
+        private string linkText;
+
+        public string URLToDownload
+        {
+            get { return linkText; }
+            set { linkText = value; OnPropertyChanged(); }
+        }
+
+
+
+    }
+}
